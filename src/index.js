8edf680fe94aa95fce1c4f4a7e8786da0cbb81c1
SanaJVN <saanaa.jvn@gmail.com>
2023-04-09 13:18:58 +0430

    Added relevant icon from API

--- src/index.js
+++ src/index.js
@@ -1,32 +1,7 @@
 function displayTemperature(response) {
-  console.log(response.data);
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.main.temp
   );
-
-  function formatDate(timestamp){
-    let date = new Date(timestamp)
-    let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thrsday",
-  "Friday",
-  "Saturday",
-  
-];
-let day = days[date.getDay()];
-let hour = date.getHours();
-if (hour < 10) {
-  hour = `0${hour}`;
-}
-let min = date.getMinutes();
-if (min < 10) {
-  min = `0${min}`;
-  }
-return`${day} ${hour}:${min}`
-
 
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#description").innerHTML =
@@ -37,10 +12,42 @@ return`${day} ${hour}:${min}`
     response.data.wind.speed
   );
 
-  document.querySelector("#date").innerHTML = formatDate;
+  document.querySelector("#date").innerHTML = formatDate(
+    response.data.dt * 1000
+  );
+
+  document
+    .querySelector("#icon")
+    .setAttribute(
+      "src",
+      `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
 }
 
-let city = "Lisbon";
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thrsday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hour = date.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let min = date.getMinutes();
+  if (min < 10) {
+    min = `0${min}`;
+  }
+  return `${day} ${hour}:${min}`;
+}
+
+let city = "New York";
 let apiKey = "f1b6473cea33289e1ae480b54c226a58";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
 axios.get(`${apiUrl}&appid=${apiKey}`).then(displayTemperature);
